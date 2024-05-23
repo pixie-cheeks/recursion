c265ee2961c4eb59cf210b8eb2d845f9cc17b7d6
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2024-05-23 14:48:51 +0530

    Uncomment logs

--- assignment-2/merge-sort.js
+++ assignment-2/merge-sort.js
@@ -23,6 +23,6 @@ function mergeSort(array) {
   return result;
 }
 
-// console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
-// console.log(mergeSort([105, 79, 100, 110]));
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
+console.log(mergeSort([105, 79, 100, 110]));
 console.log(mergeSort([5, 2, 4, 3, 7]));
